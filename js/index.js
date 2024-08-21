59148b6956ac7793bec5ba71cde5d4a223c5a625
ssiddiqa <siddiqasayma@gmail.com>
2024-08-21 16:30:41 +0600

    done

--- js/index.js
+++ js/index.js
@@ -4,12 +4,10 @@
 const loadPhone = async (ID, isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${ID}`);
     const data = await res.json();
-    console.log('object');
-
+    //console.log('object');
     const phone = data.data;
     diaplayPhones(phone, isShowAll);
 }
-
 
 //display phone
 
@@ -17,39 +15,23 @@ const diaplayPhones = (phones, isShowAll) => {
     const dataID = document.getElementById('no-data');
     const newDiv = document.getElementById('display ');
     newDiv.innerHTML = '';
-
-
-  
-
-//no data found show
-
+    //no data found show
     if (phones.length === 0) {
-        dataID.classList.remove('hidden')   ;  
-        handlSpinner(false);   
+        dataID.classList.remove('hidden');
+        handlSpinner(false);
     }
-
-       
     else {
-        const showhAllId= document.getElementById('btn-showAll');
-        if(phones.length>12  && !isShowAll ){
-
+        const showhAllId = document.getElementById('btn-showAll');
+        if (phones.length > 12 && !isShowAll) {
             showhAllId.classList.remove('hidden');
-
-           
-
         }
-        else{
+        else {
             showhAllId.classList.add('hidden');
         }
-
-      if(!isShowAll){
-        phones = phones.slice(0,12);
-
-      }
-
-
-       //show all phone in div
-
+        if (!isShowAll) {
+            phones = phones.slice(0, 12);
+        }
+        //show all phone in div
         phones.forEach(phone => {
             const phoneCard = document.createElement('div');
 
@@ -62,18 +44,13 @@ const diaplayPhones = (phones, isShowAll) => {
          <p class="secondary text-sm mb-3 mx-4">There are many variations of passages of available, but the majority have suffered</p>
          <h4 class="text-base font-bold text-[#403F3F] ">$999</h4>
          <button  onclick="handleDetails('${phone.slug}')" class="my-3 bg-color text-white text-sm font-semibold w-32 py-2 rounded mx-auto">Show Details</button>
-
-
-
            </div>
          `;
-
             newDiv.appendChild(phoneCard);
-
         });
 
         //no data found hidden
-        dataID.classList.add('hidden')   ;     
+        dataID.classList.add('hidden');
 
         //spinner hide
 
@@ -82,13 +59,10 @@ const diaplayPhones = (phones, isShowAll) => {
 
 }
 
-
-
 //search value 
 
 const handlePhone = (isShowAll) => {
     handlSpinner(true);
-
 
     const searchId = document.getElementById('searchValue');
 
@@ -96,31 +70,20 @@ const handlePhone = (isShowAll) => {
     console.log(searchValue);
 
     // searchId.value = '';
-
     loadPhone(searchValue, isShowAll);
-
 
 }
 
 // handle show all button
 
-const showAllHandle= () =>{
-
-    handlePhone(true);
-
+const showAllHandle = () => {
+    handlePhone(true)
 
 }
-
-
 
 // handle details modal 
 
 const handleDetails = async (PD) => {
-
-
-
-
-
 
     const phonDetails = await fetch(`https://openapi.programming-hero.com/api/phone/${PD}`);
     const modal = await phonDetails.json();
@@ -131,21 +94,15 @@ const handleDetails = async (PD) => {
 
     modalId.innerHTML = '';
 
-
-
-//modal
+    //modal
 
     const newModal = document.createElement('div');
-
-
-
     newModal.innerHTML = `
     <figure class="bg-[#0d6efd] bg-opacity-5 py-6 mx-2 rounded-lg">
     <img class=" w-1/3 mx-auto  my-6 "  src=${modalDetails.image} alt="">
 </figure>
 
 
-    
     <div  class="px-2">
     <h3 class="text-[#403F3F] my-3 font-bold text-xl text-left">${modalDetails.name}</h3>
     <p class="secondary text-xs text-left">It is a long established fact that a reader will
@@ -196,29 +153,16 @@ const handleDetails = async (PD) => {
 //spinner handle
 
 const handlSpinner = (isLoading) => {
-
     const spinnerId = document.getElementById('spin');
-
-
     if (isLoading === true) {
         spinnerId.classList.remove('hidden');
-
     }
     else {
         spinnerId.classList.add('hidden');
-
     }
-
-
 }
 
-
-
 // show all handle 
-
-
-
-
 loadPhone('apple', true);
 
 
